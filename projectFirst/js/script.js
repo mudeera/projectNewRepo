20cd328d26589930948e1b1c7925590fdde4fa9d
mudeera <118437152+mudeera@users.noreply.github.com>
2024-01-27 02:09:02 -0800

    updated files

--- projectFirst/js/script.js
+++ projectFirst/js/script.js
@@ -1,4 +1,53 @@
-function gotolink(link) {
-    console.log(link.value);
-    window.open(link.value);
-}
+// function gotolink(link) {
+//     console.log(link.value);
+//     window.open(link.value);
+// }
+
+const swiper = new Swiper('.swiper-first-slider', {
+    // Optional parameters
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    loop: true,
+    speed: 600,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    // breakpoints: {
+    //     480: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 24,
+    //         resistanceRatio: 0.85
+    //     },
+    //     768: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 28,
+    //         resistanceRatio: 0.85
+    //     },
+    //     980: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 28,
+    //         resistanceRatio: 0.85
+    //     },
+    //     1280: {
+    //         slidesPerView: 6,
+    //         spaceBetween: 32,
+    //         resistanceRatio : 0
+    //     }
+    // },
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
